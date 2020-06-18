a4cb5b413bba84d96e8f1b27bd9a580da75e9063
karthick-sind <30785233+karthick-sind@users.noreply.github.com>
2020-06-18 11:19:25 +0900

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ public class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Jenkins practise");
+     System.out.println("Hello Python");
     }  
     
 }  
